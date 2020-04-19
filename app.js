eb992761943ea8af511858ddfbc0036dcbf4e2a9
jackjasper <jwjasper@umich.edu>
2020-04-19 18:14:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.src = 1//cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
 };
 // Start the video stream when the window loads
